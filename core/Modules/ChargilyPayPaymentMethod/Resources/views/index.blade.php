97d6e34b5781cb7b5ee72a78e819954727e40ac6
Medboubazine <mohamedtorino161@gmail.com>
2024-08-15 07:37:35 +0100

    Add  Payment method Base

---
+++ core/Modules/ChargilyPayPaymentMethod/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('chargilypaypaymentmethod::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('chargilypaypaymentmethod.name') !!}
+    </p>
+@endsection
---
+++ core/bootstrap/cache/chargily_pay_payment_method_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\ChargilyPayPaymentMethod\\Providers\\ChargilyPayPaymentMethodServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\ChargilyPayPaymentMethod\\Providers\\ChargilyPayPaymentMethodServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
